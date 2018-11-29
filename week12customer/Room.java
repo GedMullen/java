c6a03b3a48ba83024359fcc9dbcec5bb2bcaeb4a
Ged Mullen <ged.timetabling@gmail.com>
2018-11-29 14:23:37 +0000

    latest

---
+++ week12customer/Room.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Room {
+	
+	private String type = "";
+	private ArrayList<Component> components = new ArrayList<Component>();
+	
+	public Room(String type, ArrayList<Component> components) {
+		super();
+		this.type = type;
+		this.components = components;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public ArrayList<Component> getComponents() {
+		return components;
+	}
+
+	public void setComponents(ArrayList<Component> components) {
+		this.components = components;
+	}
+
+	@Override
+	public String toString() {
+		String s = "\n" + type;
+		s += "\nComponents";
+		for (Component component : components) {
+			s+=component.toString();
+		}
+		return s;
+	}
+	
+	
+	
+	
+
+}
